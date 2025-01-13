8e0780dd541402d7c0be5bc0353800518afce776
Rsoniie <roshansoniee@gmail.com>
2025-01-13 19:30:47 +0530

    Final Backend Ready

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,3 @@
-# from flask import Flask
-# from flask_pymongo import PyMongo
-# from config import Config
-
-# app = Flask(__name__)
-# app.config.from_object(Config)
-# mongo = PyMongo(app)
-
-
-# # try:
-    
-# #     mongo.cx.admin.command('ping')
-# #     print("MongoDB connected successfully!")
-# # except Exception as e:
-# #     print("Failed to connect to MongoDB:", e)
 
 
 from flask import Flask
