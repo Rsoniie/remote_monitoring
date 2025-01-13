cc268fbe750e939eca6305a41484e69823c01a04
Rsoniie <roshansoniee@gmail.com>
2025-01-13 13:32:34 +0530

    Basic connections etc done

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+mongo = PyMongo(app)
+
+
+try:
+    
+    mongo.cx.admin.command('ping')
+    print("MongoDB connected successfully!")
+except Exception as e:
+    print("Failed to connect to MongoDB:", e)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
-from flask import render_template
-from app import app
+from flask import render_template, request, jsonify
+from app import app, mongo
 
 @app.route("/")
 def home():
     return render_template("index.html", title="Home")
+
+
+@app.route("/add_data", methods=["POST"])
+def add_data():
+    
+    data = request.json
+    username = data.get("username")
+    email = data.get("email")
+    mongo.db.users.insert_one({"username": username, "email": email})
+    return jsonify({"message": "User added successfully!"})
