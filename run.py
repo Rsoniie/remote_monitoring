9a07d566059b6d51cffbff22d7e565c41e9a8eca
Rsoniie <roshansoniee@gmail.com>
2025-01-14 22:34:01 +0530

    Done

--- run.py
+++ run.py
@@ -1,14 +1,7 @@
-# from app import app
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
 
 
 import os
 from app import app
-# from flask_cors import CORS
-
-
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000)) 
