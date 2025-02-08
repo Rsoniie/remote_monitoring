4d717adb7bbf2267dba84e780bd793910d3e8b9f
Rsoniie <roshansoniee@gmail.com>
2025-02-08 21:26:10 +0530

    Log In function compeleted from client and server

--- app/static/script.js
+++ app/static/script.js
@@ -1,6 +1,41 @@
-const show_btn = document.querySelector('#show')
-show_btn.addEventListener('click', () => {
-    console.log("Show button is clicked");
-    
+const login = document.querySelector("#login");
+const form = document.querySelector("#form");
+const user = document.querySelector("#user");
+const pass = document.querySelector("#pass");
+const submit = document.querySelector("#submit");
+form.style.display = "none";
 
-})
+login.addEventListener("click", (event) => {
+  event.preventDefault();
+  form.style.display = "block";
+});
+submit.addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  const username = user.value;
+  const password = pass.value;
+
+  form.style.display = "none";
+
+  try {
+    const response = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Enter correct credentials: ${response.status}`);
+    }
+
+    const data = await response.json();
+    localStorage.setItem("username", data.username);
+    console.log(data.username);
+    console.log(data.message);
+  } catch (error) {
+    console.error("Error:", error);
+    alert(error.message);
+  }
+});
